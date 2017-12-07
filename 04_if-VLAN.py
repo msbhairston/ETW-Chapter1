042bc084a5eab2ae93e84ea2ae2ed284e87ce451
attc-249-02 <attc-249-02@tri-c.edu>
2017-12-07 13:12:53 -0500

    poached ETW Code Chapter1

---
+++ 04_if-VLAN.py
@@ -0,0 +1,8 @@
+nativeVLAN = 1
+dataVLAN = 100
+
+if nativeVLAN == dataVLAN:
+    print("The native VLAN and the data VLAN are the same.")
+else:
+    print("This native VLAN and the data VLAN are different.")
+    
